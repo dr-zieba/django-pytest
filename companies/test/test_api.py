07f2f6fc835a4c6b2fb4aefb17cd920f56f0e4f1
dr-zieba <zieba1jan@gmail.com>
2022-12-12 22:03:02 +0100

    added pytest style tests

--- companies/test/test_api.py
+++ companies/test/test_api.py
@@ -1,5 +1,3 @@
-from unittest import TestCase
-from django.test import Client
 from django.urls import reverse
 from django.conf import settings
 import json
@@ -7,7 +5,6 @@ import logging
 import django_pytest
 from companies.models import Company
 import pytest
-import inspect
 
 if not settings.configured:
     settings.configure(django_pytest, DEBUG=True)
@@ -24,73 +21,72 @@ logger.info("************")
 logger.info("Starting testing...")
 logger.info("************")
 
-
-@pytest.mark.django_db
-class BasiCompanyAPITestCase(TestCase):
-    def setUp(self) -> None:
-        self.client = Client()
-        self.companies_url = reverse("companies-list")
-
-    def tearDown(self) -> None:
-        pass
+companies_url = reverse("companies-list")
+# pytestmark will be applied to every function in file
+pytestmark = pytest.mark.django_db
 
 
-class TestGetCOmpanies(BasiCompanyAPITestCase):
-    def test_zero_comapnies_return_empty_list(self) -> None:
-        response = self.client.get(self.companies_url)
-        self.assertEqual(response.status_code, 200)
-        self.assertEqual(json.loads(response.content), [])
-
-    def test_one_company_exists_should_success(self) -> None:
-        test_company = Company.objects.create(name="XXXAAA")
-        response = self.client.get(self.companies_url)
-        response_content = json.loads(response.content)[0]
-        self.assertEqual(response.status_code, 200)
-        self.assertEqual(response_content.get("name", None), test_company.name)
-        self.assertEqual(response_content.get("status", None), "Hiring")
-
-        # test_company.delete()
+"""*********TEST GET COMPANIES**********"""
 
 
-@pytest.mark.django_db
-class TestPostCompanies(BasiCompanyAPITestCase):
-    def test_create_company_without_args(self) -> None:
-        response = self.client.post(self.companies_url)
-        self.assertEqual(response.status_code, 400)
-        self.assertEqual(
-            json.loads(response.content).get("name", None)[0], "This field is required."
-        )
+def test_zero_companies_return_empty_list(client) -> None:
+    response = client.get(companies_url)
+    assert response.status_code == 200
+    assert json.loads(response.content) == []
 
-    def test_create_existing_comapny_should_fail(self) -> None:
-        Company.objects.create(name="Opel")
-        response = self.client.post(self.companies_url, data={"name": "Opel"})
-        self.assertEqual(response.status_code, 400)
-        self.assertEqual(
-            json.loads(response.content).get("name", None)[0],
-            "company with this name already exists.",
-        )
 
-    def test_create_company_only_with_name(self) -> None:
-        response = self.client.post(self.companies_url, data={"name": "BMW"})
-        self.assertEqual(response.status_code, 201)
-        response_content = json.loads(response.content)
-        self.assertEqual(response_content.get("name"), "BMW")
-        self.assertEqual(response_content.get("status"), "Hiring")
-        self.assertEqual(response_content.get("application_link"), "")
-        self.assertEqual(response_content.get("notes"), "")
+def test_one_company_exists_should_success(client) -> None:
+    test_company = Company.objects.create(name="XXXAAA")
+    response = client.get(companies_url)
+    response_content = json.loads(response.content)[0]
+    assert response.status_code == 200
+    assert response_content.get("name", None) == test_company.name
+    assert response_content.get("status", None) == "Hiring"
+    test_company.delete()
 
-    def test_create_comapny_with_layoffs_status_should_success(self) -> None:
-        response = self.client.post(
-            self.companies_url, data={"name": "Mercedes", "status": "Layoffs"}
-        )
-        self.assertEqual(response.status_code, 201)
-        response_content = json.loads(response.content)
-        self.assertEqual(response_content.get("name"), "Mercedes")
-        self.assertEqual(response_content.get("status"), "Layoffs")
 
-    def test_create_company_with_wrong_status(self) -> None:
-        response = self.client.post(
-            self.companies_url, data={"name": "Audi", "status": "layoffs111"}
-        )
-        self.assertEqual(response.status_code, 400)
-        self.assertIn("is not a valid ", str(response.content))
+"""*********TEST POST COMPANIES**********"""
+
+
+def test_create_company_without_args(client) -> None:
+    response = client.post(companies_url)
+    assert response.status_code == 400
+    assert (
+        json.loads(response.content).get("name", None)[0] == "This field is required."
+    )
+
+
+def test_create_existing_comapny_should_fail(client) -> None:
+    Company.objects.create(name="Opel")
+    response = client.post(companies_url, data={"name": "Opel"})
+    assert response.status_code == 400
+    assert (
+        json.loads(response.content).get("name", None)[0]
+        == "company with this name already exists."
+    )
+
+
+def test_create_company_only_with_name(client) -> None:
+    response = client.post(companies_url, data={"name": "BMW"})
+    assert response.status_code == 201
+    response_content = json.loads(response.content)
+    assert response_content.get("name") == "BMW"
+    assert response_content.get("status") == "Hiring"
+    assert response_content.get("application_link") == ""
+    assert response_content.get("notes") == ""
+
+
+def test_create_comapny_with_layoffs_status_should_success(client) -> None:
+    response = client.post(
+        companies_url, data={"name": "Mercedes", "status": "Layoffs"}
+    )
+    assert response.status_code == 201
+    response_content = json.loads(response.content)
+    assert response_content.get("name") == "Mercedes"
+    assert response_content.get("status") == "Layoffs"
+
+
+def test_create_company_with_wrong_status(client) -> None:
+    response = client.post(companies_url, data={"name": "Audi", "status": "layoffs111"})
+    assert response.status_code == 400
+    assert "is not a valid " in str(response.content)
--- companies/views.py
+++ companies/views.py
@@ -1,4 +1,3 @@
-from django.shortcuts import render
 from rest_framework.viewsets import ModelViewSet
 from .serializer import CompanySerializer
 from .models import Company
